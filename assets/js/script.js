959fa26ce6e64bd8d2b60822708bb383a280abae
Gregory Casanova <gregory.casanova@alumnos.usm.cl>
2021-06-09 22:28:18 -0400

    Finalizado de proyecto con estilo CSS, link a rrss, JQuery, Bootstrap y Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,38 @@
+//Cambio de color de la barra de navegación
+function myFunction(x) {
+  if (x.matches) {
+    $(document).ready(function() {
+      $( window ).scroll(function() {
+        if($( window ).scrollTop()>($( "#presentacion" ).offset().top - $( "#navegador" ).height())) {
+          $( "#navegador" ).removeClass( "navstyle" ).addClass( "newnavstyle" );
+        } else {
+          $( "#navegador" ).removeClass( "newnavstyle" ).addClass( "navstyle" );
+        }
+      });
+    });
+  }
+}
+var x = window.matchMedia("(min-width: 768px)")
+myFunction(x)
+x.addListener(myFunction)
 
+//Smooth scrolling
+$(document).ready(function(){
+  $("nav a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1600, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+//Tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
